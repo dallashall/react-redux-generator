89927bb57b3e7b99e9691a7ea743fc1ad80accf7
David Dobrynin <david.dobrynin@gmail.com>
2017-06-03 16:48:48 -0700

    Creates entry file
    Injects the React virtual DOM into the 'root' HTML element and
    initializes the Redux Store.

---
+++ docs/example_app/sample/frontend/example.jsx
@@ -0,0 +1,13 @@
+// React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components
+import Root from './components/root'
+import configureStore from './store/store'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  const root = document.getElementById('root');
+  REACTDOM.render(<Root store={store} />, root)
+})
